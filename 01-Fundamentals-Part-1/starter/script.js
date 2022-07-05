cfe2cb183ea5e7c4cf0176f4d8d9e0d460626ae4
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-04 22:48:44 -0300

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,26 +1,3 @@
-
-const now = 2037;
-const ageJonas = now - 1995;
-const ageSarah = now - 2018;
-console.log(ageJonas,ageSarah);
-
-
-console.log(ageJonas * 2,ageJonas / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2;
-
-const firstName = 'Jonas';
-const lastName = 'Scheedtnann';
-console.log(firstName + ' ' + lastName);
-
-let x = 10 + 5;
-x += 10;// x = x + 10 -> 25
-x *= 4;//x = x * 10 -> 100
-x ++;//x = x + 1
-x --;//x = x - 1;
-console.log(x);
-
-console.log(ageJonas > ageSarah);// > , < , >= , <=
-console.log(ageSarah >= 18);
-
-const isFullAge = ageSarah >= 18;
-console.log(now - 1991 > now - 2018)
+let x,y;
+x = y = 25 - 10 -5;
+console.log(x,y);
