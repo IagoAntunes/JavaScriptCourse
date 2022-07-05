9a5ad48bae0fd17d0f0c4a0a4770c61089b3ad86
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-04 21:59:47 -0300

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,19 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
 
-let firstName = "Matilda";
-let first = 'Jonas';
-let firstNamePerson;
-let first_name_person;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
-console.log(firstName);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
