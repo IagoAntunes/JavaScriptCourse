8f9e9186eb41d9356c7e9ca4c8cac6585aec2d79
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-05 21:53:40 -0300

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,20 @@
-'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :D');
+function logger(){
+    console.log("My name is Jonas");
+}
 
-const interface = 'Audio';
-const private = 534;
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples,oranges){
+    console.log(apples,oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(5));
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
