51dfc06b606fdd90923f4166585b59a21769a473
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-05 21:36:12 -0300

    Logical Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,15 @@
-//Boolean Logic
+const hasDriversLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const isTired =true;
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired){
+    console.log("Sarah is able to drive!");
+}else{
+    console.log("Someone else should drive");
+}
