114eed998ae25d7fe2adf10c7725452e06a63078
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-05 21:16:30 -0300

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,20 +1,27 @@
-//Type Conversion
-const inputYear = '1991';
-console.log(Number(inputYear),inputYear);
-console.log(Number(inputYear) + 18);
 
-console.log(Number('Jonas'));
-console.log(typeof NaN);
-
-console.log(String(23 + 5),56);
-
-//Type Coercion
-console.log("I am " + 23 + "years old");
-console.log("23" + "10" - 3);
-console.log("23" / "10");
+// 0
+// ''
+// undefined
+// null
+// NaN
 
 
-let n = "1" + 1;
-n = n - 1;
-console.log(n);
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
 
+const money = 100;
+if(money){
+    console.log("Don't spend it all");
+}else{
+    console.log("You should get a job");
+}
+
+let height;
+if(height){
+    console.log("YAY!Height is defined")
+}else{
+    console.log("Height is UNDEFINED");
+}
