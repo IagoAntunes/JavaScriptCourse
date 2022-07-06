f68d1a3d2d48279d238becdf5a99dd537fc0f429
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-05 21:40:03 -0300

    The Conditional (Ternary) Operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,15 @@
-3 + 4
-1991
-true && false && !false
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
 
-if (23 > 10) {
-  const str = '23 is bigger';
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
 }
+console.log(drink2);
 
-const me = 'Jonas';
-console.log(`I'm ${2037 - 1991} years old ${me}`);
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
