8ad19b3ef5d237f55f58645a7e6b89837a29542b
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-10 23:21:08 -0300

    Guess my Number

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,18 +1,1 @@
-
-
-
-
-
-
-const yearsUntilRetirement = (birthYeah,firstName) =>{
-    const age = 2037 - birthYeah;
-    const retirement = 65 - age;
-    return `${firstName} retires in ${retirement} years`;
-}
-
-console.log(yearsUntilRetirement(1991,"Joao"));
-console.log(yearsUntilRetirement(2022,"Bob",56));
-
-const t1 = teste => 20 - 16;
-
-console.log(t1());
+document.querySelector("aaa");
