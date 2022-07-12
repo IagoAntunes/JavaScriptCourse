46a98f527d47f507b0f2a127836516ce627edd39
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-11 22:12:48 -0300

    Modal Window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -11,7 +11,6 @@ const closeModal = function(){
     overlay.classList.add('hidden');
 }
 const openModal = function(){
-    console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
@@ -24,3 +23,9 @@ for(let i = 0;i < btnsOpenModal.length;i++){
 btnCloseModal.addEventListener('click',closeModal);
 
 overlay.addEventListener('click',closeModal);
+
+
+document.addEventListener('keydown',function(e){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden'))
+        closeModal();
+});
